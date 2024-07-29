a6e834ecf3d2d5f92bfe6dac52b613984f22bc52
Pablo Garay <elxebec@gmail.com>
2024-07-29 19:16:27 -0300

    Fix verify token method

--- modulo 04/demo-server/src/middlewares/verify-token.js
+++ modulo 04/demo-server/src/middlewares/verify-token.js
@@ -1,19 +1,31 @@
 const jwt = require('jsonwebtoken');
+const { User } = require('../models');
 
 const { JWT_SECRET } = process.env
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   try {
-    const token = req.header('Authorization');
-
-    if (!token) {
-      return res.status(401).json({ message: "Token not provied" });
+    const authorizationHeader = req.headers.authorization;
+    if(!authorizationHeader || !authorizationHeader.startsWith("Bearer") ) {
+        return res.status(401).json({ error: "You are not authorized to access this. No token"})
     }
 
-    jwt.verify(token, JWT_SECRET, (err, user) => {
-      req.user = user;
-      next();
-    });
+    const token = authorizationHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if(!decoded || !decoded.id ) {
+        return res.status(401).json({ error: "Invalid token"})
+    }
+
+    const user = await User.findByPk(decoded.id)
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    req.user = user;
+    delete req.user.password;
+
+    next();
   } catch (error) {
     return res.status(403).json({ message: "Token not valid" });
   }
