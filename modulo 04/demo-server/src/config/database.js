825556eac49b1bc1f1a4ba9a39379f4d65ae5324
Pablo Garay <elxebec@gmail.com>
2024-07-28 23:44:16 -0300

    Desactivate server logging

--- modulo 04/demo-server/src/config/database.js
+++ modulo 04/demo-server/src/config/database.js
@@ -24,7 +24,8 @@ const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT } = process.env
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   port: DB_PORT,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  logging: false
 });
 
 sequelize.authenticate()
